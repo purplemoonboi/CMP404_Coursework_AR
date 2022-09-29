716a5ccc01bf402ef49a7419cee161327f4b127f
Rhys <47715008+purplemoonboi@users.noreply.github.com>
2022-09-29 10:56:16 +0100

    Working on wrapping up lab 02

---
+++ Source/CMP404_AR/CMP404_AR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CMP404_AR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CMP404_AR, "CMP404_AR" );
---
+++ Source/CMP404_AR/Private/CustomPawn.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CustomPawn.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "ARBlueprintLibrary.h"
+
+// Sets default values
+ACustomPawn::ACustomPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACustomPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UKismetSystemLibrary::PrintString(this, FString(TEXT("Hello world")), true, true, FLinearColor::Red, 5);
+
+	UARSessionConfig* Config = NewObject<UARSessionConfig>();
+	UARBlueprintLibrary::StartARSession(Config);
+}
+
+// Called every frame
+void ACustomPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACustomPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
